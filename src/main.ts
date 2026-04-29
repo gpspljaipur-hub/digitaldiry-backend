dfaaafa1de99150aa57f2bda31e29f9c4b822552
apple <apple@apples-MacBook-Pro.local>
2026-04-29 12:35:41 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(process.env.PORT ?? 3000);
-//   console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
-// }
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -14,9 +8,8 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
 
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port, '0.0.0.0'); // 👈 IMPORTANT
 
   console.log(`Server running on port ${port}`);
 }
-bootstrap();
 bootstrap();
